2554c242f032495ad812d86ab5d24e46c1d58813
flffkd12 <flffkd12@gmail.com>
2025-02-20 18:16:44 +0900

    feat: 구글 로그인 버튼 생성 및 인증코드 받아오기
    * Custom Tab 제거, 인 앱 방식 사용

--- app/src/main/java/com/example/myschedule/login/Login.kt
+++ app/src/main/java/com/example/myschedule/login/Login.kt
@@ -1,57 +1,127 @@
 package com.example.myschedule.login
 
-import android.net.Uri
-import androidx.browser.customtabs.CustomTabsIntent
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myschedule.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 
 @Composable
 fun Login() {
-    val context = LocalContext.current
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
         Spacer(Modifier.weight(1f))
-        Text(text = "My Schedule", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        Text(
+            text = stringResource(R.string.app_name),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
         Spacer(Modifier.height(4.dp))
-        Text(text = "오늘의 일정 가이드", fontSize = 20.sp, fontWeight = FontWeight.Medium)
+        Text(
+            text = stringResource(R.string.app_description),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium
+        )
         Spacer(Modifier.height(12.dp))
         Image(
             painter = painterResource(R.drawable.logo),
-            contentDescription = "logo",
+            contentDescription = stringResource(R.string.app_logo),
             modifier = Modifier.fillMaxWidth(0.4f).aspectRatio(1.25f)
         )
         Spacer(Modifier.weight(2f))
-        Image(
-            painter = painterResource(R.drawable.google_login),
-            contentDescription = "logo",
-            modifier = Modifier.fillMaxWidth(0.7f).aspectRatio(4.73f)
-                .clickable {
-                    val customTabsIntent = CustomTabsIntent.Builder().build()
-                    customTabsIntent.launchUrl(
-                        context,
-                        Uri.parse("http://192.168.219.101:8080/oauth2/authorization/google")
-                    )
-                }
-        )
+        GoogleSignInButton()
         Spacer(Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun GoogleSignInButton() {
+    val context = LocalContext.current
+    val gso = remember {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestServerAuthCode(context.getString(R.string.google_client_id))
+            .build()
+    }
+
+    val googleSignInClient = remember { GoogleSignIn.getClient(context, gso) }
+    var authCode: String?
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                authCode = account.serverAuthCode
+                println("Login successful. Authorization code: $authCode")
+            } catch (e: ApiException) {
+                authCode = null
+                println("Login failed.")
+                println("1. Check the 'Client ID' if it matches with Google Cloud Console.")
+                println("2. Check the latest authorization dependency.")
+                println("3. If none of it than check the error code: ${e.statusCode}")
+            }
+        } else {
+            authCode = null
+            println("Login failed.")
+            println("1.Maybe user canceled login process.")
+            println("2.If not, Refactor code related to OAuth authorization.")
+        }
+    }
+
+    Button(
+        onClick = {
+            val signInIntent = googleSignInClient.signInIntent
+            launcher.launch(signInIntent)
+        },
+        shape = RoundedCornerShape(4.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black.copy(0.54f)
+        ),
+        border = BorderStroke(width = 1.dp, color = Color(0xFF747775)),
+        contentPadding = PaddingValues(8.dp),
+        modifier = Modifier.height(48.dp).fillMaxWidth(0.6f),
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(R.drawable.google_logo),
+                contentDescription = stringResource(R.string.google_logo),
+                modifier = Modifier.size(32.dp)
+            )
+            Spacer(Modifier.weight(1f))
+            Text(text = stringResource(R.string.google_login_button_name), fontSize = 16.sp)
+            Spacer(Modifier.weight(1f))
+        }
     }
 }
