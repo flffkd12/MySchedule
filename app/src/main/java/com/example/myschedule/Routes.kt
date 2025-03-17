03c6bf5757e379146518717c7a7a45e50f846c86
flffkd12 <flffkd12@gmail.com>
2025-03-18 02:13:15 +0900

    docs: 화면 경로 설명 주석 추가

--- app/src/main/java/com/example/myschedule/Routes.kt
+++ app/src/main/java/com/example/myschedule/Routes.kt
@@ -1,10 +1,21 @@
 package com.example.myschedule
 
 object Routes {
+    /** 로그인 화면 경로 */
     const val LOGIN = "login"
+
+    /** 메인 화면 경로 */
     const val MAIN_SCREEN = "main_screen"
+
+    /** 일정 생성 화면 경로 */
     const val CREATE_SCHEDULE = "create_schedule"
+
+    /** 일정 제목과 시간 입력 화면 경로 */
     const val CREATE_TITLE_AND_TIME = "create_title_and_time"
+
+    /** 월별 선택 날짜의 일정 확인 화면 */
     const val MONTHLY_SCHEDULE = "monthly_schedule"
+
+    /** 일정 수정 화면 */
     const val MODIFY_SCHEDULE = "modify_schedule"
 }
