d001f5839c8948694c5ad4a5c5ca5a9ce6cdab1a
flffkd12 <flffkd12@gmail.com>
2025-05-05 16:41:58 +0900

    feat: 메인화면 지역 날씨 UI 완성
    * 날씨 이미지, 기온, 강수/적설 확률, 강수/적설량 추가

--- app/src/main/java/com/example/myschedule/mainscreen/MainScreen.kt
+++ app/src/main/java/com/example/myschedule/mainscreen/MainScreen.kt
@@ -2,12 +2,14 @@ package com.example.myschedule.mainscreen
 
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -18,11 +20,15 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.myschedule.BtmNavBar
+import com.example.myschedule.R
 import com.example.myschedule.Routes
+import com.example.myschedule.data.WeatherCode
 import com.example.myschedule.data.WeatherDto
+import com.example.myschedule.data.toWeatherCode
 import com.example.myschedule.ui.theme.*
 import com.example.myschedule.viewmodels.MonthlyScheduleViewModel
 import com.example.myschedule.viewmodels.UserViewModel
@@ -137,6 +143,7 @@ fun RegionWeatherUI(weatherList: List<WeatherDto>) {
                 val hour = weatherDto.time.substring(0, 2)
 
                 Column(
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.padding(horizontal = 8.dp, vertical = 12.dp).width(48.dp)
                 ) {
@@ -162,16 +169,82 @@ fun RegionWeatherUI(weatherList: List<WeatherDto>) {
 
                     Text(
                         text = label,
-                        style = MaterialTheme.typography.bodySmall,
-                        color = Black
+                        color = Blue,
+                        style = MaterialTheme.typography.bodySmall
                     )
 
                     // 2행: 시간 라벨
                     val timeLabel = if (isCurrentHour) "지금" else "${hour}시"
                     Text(
                         text = timeLabel,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = Black
+                        color = Black,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+
+                    // 3행: 날씨 표시
+                    when (weatherDto.toWeatherCode()) {
+                        WeatherCode.SUNNY -> Image(
+                            painter = painterResource(R.drawable.sunny),
+                            contentDescription = null,
+                            modifier = Modifier.size(32.dp)
+                        )
+
+                        WeatherCode.SUNNY_WITH_CLOUD -> Image(
+                            painter = painterResource(R.drawable.little_cloudy),
+                            contentDescription = null,
+                            modifier = Modifier.size(32.dp)
+                        )
+
+                        WeatherCode.CLOUDY -> Image(
+                            painter = painterResource(R.drawable.cloudy),
+                            contentDescription = null,
+                            modifier = Modifier.size(32.dp)
+                        )
+
+                        WeatherCode.RAINY -> Image(
+                            painter = painterResource(R.drawable.rainy),
+                            contentDescription = null,
+                            modifier = Modifier.size(32.dp)
+                        )
+
+                        WeatherCode.SNOWY -> Image(
+                            painter = painterResource(R.drawable.snowy),
+                            contentDescription = null,
+                            modifier = Modifier.size(32.dp)
+                        )
+
+                        else -> Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null,
+                            tint = Black,
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
+
+                    // 4행: 기온 표시
+                    Text(
+                        text = "${weatherDto.temperature}º",
+                        color = Black,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+
+                    // 5행: 강수, 적설 확률
+                    Text(
+                        text = "${weatherDto.rainProbability}%",
+                        color = if (weatherDto.rainProbability.toInt() >= 60) Blue else Gray,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+
+                    // 6행: 강수량, 적설량
+                    val isRainy = weatherDto.rainAmount != "강수없음"
+                    val isSnowy = weatherDto.snowAmount != "적설없음"
+                    Text(
+                        text = if (!isRainy && !isSnowy) ""
+                        else if (isRainy) "${weatherDto.rainAmount}mm"
+                        else "${weatherDto.snowAmount}mm",
+                        color = if (isRainy || isSnowy) Blue else Transparent,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.background(VeryLightBLue)
                     )
                 }
             }
