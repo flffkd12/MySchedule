bea8cec9cec477c4841268c6322f6222368f376e
flffkd12 <flffkd12@gmail.com>
2025-05-05 17:08:15 +0900

    refactor: 날씨 요청에 필요한 날짜, 시간 반환 메서드 개선

--- app/src/main/java/com/example/myschedule/util/WeatherInfoHelper.kt
+++ app/src/main/java/com/example/myschedule/util/WeatherInfoHelper.kt
@@ -12,36 +12,17 @@ object WeatherInfoHelper {
 
     fun getCurrentBaseDateTime(): Pair<String, String> {
         val currentTime = LocalDateTime.now()
-        val validTimes = listOf("0200", "0500", "0800", "1100", "1400", "1700", "2000", "2300")
+        val validTimes = listOf("2300", "0200", "0500", "0800", "1100", "1400", "1700", "2000")
+        val validTimesIndex = currentTime.hour / 3
 
-        // 현재 시간 문자열 생성
-        val currentHourStr = currentTime.format(DateTimeFormatter.ofPattern("HH00"))
-        var closestIndex = validTimes.indexOfLast { it <= currentHourStr }
-
-        // 현재 시간이 0~2시인 경우
-        if (closestIndex == -1) {
-            return Pair(
+        return if (validTimesIndex == 0) {
+            Pair(
                 currentTime.minusDays(1).format(DateTimeFormatter.BASIC_ISO_DATE),
-                validTimes.last()
+                validTimes[validTimesIndex]
             )
+        } else {
+            Pair(currentTime.format(DateTimeFormatter.BASIC_ISO_DATE), validTimes[validTimesIndex])
         }
-
-        // 현재 시각이 validTimes 정각 ~ 정각 + 10분 인지 확인
-        val baseTime = currentTime.withHour(3 * closestIndex + 2)
-        if (currentTime.isAfter(baseTime) && currentTime.isBefore(baseTime.plusMinutes(10))) {
-            closestIndex -= 1
-            if (closestIndex == -1) {
-                return Pair(
-                    currentTime.minusDays(1).format(DateTimeFormatter.BASIC_ISO_DATE),
-                    validTimes.last()
-                )
-            }
-        }
-
-        return Pair(
-            currentTime.format(DateTimeFormatter.BASIC_ISO_DATE),
-            validTimes[closestIndex]
-        )
     }
 
     fun getMostDangerousWeatherCode(
